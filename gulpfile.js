ccaeea6c0a6fd3e2279b8a7ee64242b7245f83b1
Nidhish Thomas <nidhishthomas@Nidhishs-MBP.fritz.box>
2019-07-12 13:17:08 +0200

    website works first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+
+/*
+gulp.task('sass', () => 
+    gulp.src('src/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/styles'))
+);
+*/
+
